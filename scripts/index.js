3d4896aa26efa563033fd6740eb8ebeae3eaef58
Mark Bolsover <markbolsover@outlook.com>
2022-06-01 12:12:49 +0100

    update to home-town

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+var nav = document.getElementById('nav');
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+		return;
+	}
+	
+	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+	var elementHeight = element.offsetHeight;
+	var scrollTop = document.documentElement.scrollTop;
+	
+	var opacity = 1;
+	
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+	}
+	
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(nav);
+}
+
+window.addEventListener('scroll', scrollHandler);
